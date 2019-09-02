10901bef7c549ec21324c3e0870ef18e18acd9ab
sumancrest0001 <suman.crest0001@gmail.com>
2019-09-03 00:30:41 +0545

    completed all the methods and linter files added

--- main.rb
+++ main.rb
@@ -58,29 +58,33 @@ module Enumerable
     end
     puts count
   end
-
-  def my_map
+  #initial version of my_map method
+=begin
+  def my_map(&procs)
     output = []
-    if block_given?
-      self.my_each { |ele| output << yield(ele)}
-    end
+      self.my_each { |ele| output << procs.call(ele)}
+     output
+  end
+=end
+  def my_map(&procs)
+    output = []
+      self.my_each { |ele| output <<(procs.nil? ? yield(ele) : procs.call(ele))}
      output
   end
 
-  def my_inject()
-    sum = 0
+  def my_inject(x)
+    sum = x
     self.my_each do |ele|
       sum = yield(ele, sum)
     end
     puts sum
   end
-
   def multiply_els
-    self.my_each { |ele| self.my_inject }
-    end
+    my_inject(1){|ele, sum| sum * ele}
+  end
 end
-
 array = [1, 3, 24, 6, 2, 3, 3, 3, 29, 10, 1] # total sum = 85
+modified_map = Proc.new {|ele| ele * ele }
 #array.my_each {|i| puts "doubled number is: #{i * 2}"}
 #array.my_each_with_index {|number, i| puts "index is #{i} and element is #{number}"}
 #array.my_select {|ele| ele.odd?}
@@ -88,5 +92,7 @@ array = [1, 3, 24, 6, 2, 3, 3, 3, 29, 10, 1] # total sum = 85
 #array.my_any? {|ele| ele >= 20}
 #array.my_none? {|ele| ele < 1}
 #array.my_count(3) { |ele, x| ele == x}
-#array.my_map {|ele| ele * ele}
+puts array.my_map {|ele| ele * ele}
+#puts array.my_map(&modified_map)
 #array.my_inject {|ele, sum| sum += ele}
+#array.multiply_els {|ele, sum| sum * ele}
