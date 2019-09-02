2c3f7959ec30debc2007e3d2f9f2435c3bd9ff38
sumancrest0001 <suman.crest0001@gmail.com>
2019-09-03 02:19:36 +0545

    resolving errors

--- main.rb
+++ main.rb
@@ -1,34 +1,39 @@
+# frozen_string_literal: true
 module Enumerable
   def my_each
-    for i in (0...self.size)
-      yield(self[i])
+    counter=0
+    while counter < self.size
+      yield(self[counter])
+      counter +=1
     end
   end
+
   def my_each_with_index
-    for i in 0...self.length
+    i = 0
+    while i < self.length
       yield(self[i], i)
     end
   end
 
   def my_select
     output = []
-    self.my_each {|ele| odd_numbers << ele if yield(ele)}
+    my_each { |ele| output << ele if yield(ele) }
     output
   end
 
   def my_all?
     output = true
-    self.my_each do |ele|
+    my_each do |ele|
        if yield(ele) != true
          output = false
        end
-     end
+    end
     output
   end
 
   def my_any?
     output = false
-    self.my_each do |ele|
+    my_each do |ele|
        if yield(ele) == true
          output = true
        end
@@ -38,26 +43,27 @@ module Enumerable
 
   def my_none?
     output = true
-    self.my_each do |ele|
+    my_each do |ele|
        if yield(ele) != false
          output = false
        end
-     end
+    end
     puts output
   end
 
   def my_count(x = nil)
     count = 0
     if x == nil
-      self.my_each { |ele| count +=1 }
+      my_each { |ele| count +=1 }
     end
-    self.my_each do |ele|
+    my_each do |ele|
       if yield(ele, x)
         count += 1
       end
     end
     puts count
   end
+
   #initial version of my_map method
 =begin
   def my_map(&procs)
@@ -66,33 +72,35 @@ module Enumerable
      output
   end
 =end
+
   def my_map(&procs)
     output = []
-      self.my_each { |ele| output <<(procs.nil? ? yield(ele) : procs.call(ele))}
+      my_each { |ele| output <<(procs.nil? ? yield(ele) : procs.call(ele))}
      output
   end
 
   def my_inject(x)
     sum = x
-    self.my_each do |ele|
+    my_each do |ele|
       sum = yield(ele, sum)
     end
     puts sum
   end
+
   def multiply_els
-    my_inject(1){|ele, sum| sum * ele}
+    my_inject(1){ |ele, sum| sum * ele }
   end
 end
 array = [1, 3, 24, 6, 2, 3, 3, 3, 29, 10, 1] # total sum = 85
-modified_map = Proc.new {|ele| ele * ele }
-#array.my_each {|i| puts "doubled number is: #{i * 2}"}
-#array.my_each_with_index {|number, i| puts "index is #{i} and element is #{number}"}
-#array.my_select {|ele| ele.odd?}
-#array.my_all? {|ele| ele >= 2}
-#array.my_any? {|ele| ele >= 20}
-#array.my_none? {|ele| ele < 1}
-#array.my_count(3) { |ele, x| ele == x}
-puts array.my_map {|ele| ele * ele}
-#puts array.my_map(&modified_map)
-#array.my_inject {|ele, sum| sum += ele}
-#array.multiply_els {|ele, sum| sum * ele}
+modified_map = Proc.new { |ele| ele * ele }
+# array.my_each { |i| puts "doubled number is: #{i * 2}" }
+# array.my_each_with_index { |number, i| puts "index is #{i} and element is #{number}"}
+# puts array.my_select { |ele| ele.odd? }
+# array.my_all? { |ele| ele >= 2 }
+# array.my_any? { |ele| ele >= 20 }
+# array.my_none? { |ele| ele < 1 }
+# array.my_count(3) { |ele, x| ele == x }
+puts array.my_map { |ele| ele * ele }
+puts array.my_map(&modified_map)
+# array.my_inject { |ele, sum| sum += ele }
+# array.multiply_els { |ele, sum| sum * ele }
