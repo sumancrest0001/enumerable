af8a726ab80b8c2af816d03b57eec7c7c32aac53
sumancrest0001 <suman.crest0001@gmail.com>
2019-09-26 03:09:40 +0545

    solving linter errors

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -3,7 +3,7 @@
 require_relative '../main'
 RSpec.describe 'Enumerable' do
   let(:test_arr) { [1, 3, 6, 9, 10] }
-  let(:newArr){ [] }
+  let(:newArr) { [] }
 
   describe '#my_each' do
     context 'when input is valid' do
@@ -17,12 +17,12 @@ RSpec.describe 'Enumerable' do
   describe '#my_each_with_index' do
     context 'when input is valid' do
       it 'should display array of index' do
-        test_arr.my_each_with_index { |n, i| newArr << i }
+        test_arr.my_each_with_index { |i| newArr << i }
         expect(newArr).to eql([0, 1, 2, 3, 4])
       end
 
       it 'should display array of elements' do
-        test_arr.my_each_with_index { |n, i| newArr << n }
+        test_arr.my_each_with_index { |n| newArr << n }
         expect(newArr).to eql(test_arr)
       end
     end
@@ -59,21 +59,21 @@ RSpec.describe 'Enumerable' do
   describe '#my_all? method' do
     context 'when the array do not contain false or nil' do
       it 'should give true' do
-        expect(test_arr.my_all?{|n| n == false || n == nil}).to be true
+        expect(test_arr.my_all? { |n| n == false || n.nil? }).to be true
       end
     end
 
     context 'when at least one element is false or nil' do
-      let(:arr){[2, 3, false]}
+      let(:arr) { [2, 3, false] }
       it 'should return false' do
-        expect(arr.my_all?{|n| n == false || n == nil}).to be false
+        expect(arr.my_all? { |n| n == false || n.nil? }).to be false
       end
     end
 
     context 'when all the elements are either false or nil' do
-      let(:arr){[nil, false, false]}
+      let(:arr) { [nil, false, false] }
       it 'should return false' do
-        expect(arr.my_all?{|n| n == false || n == nil}).to be false
+        expect(arr.my_all? { |n| n == false || n.nil? }).to be false
       end
     end
 
@@ -91,8 +91,8 @@ RSpec.describe 'Enumerable' do
       end
     end
 
-    context ' when one element is zero' do
-      let(:arr ) {[1, 3, 0]}
+    context 'when one element is zero' do
+      let(:arr) { [1, 3, 0] }
       it 'should be equal to zero' do
         expect(arr.multiply_els { |sum, ele| sum * ele }).to be 0
       end
@@ -116,7 +116,7 @@ RSpec.describe 'Enumerable' do
     describe 'block is given' do
       context 'when valid input is given' do
         it 'returns repetition of a particular number' do
-          expect(test_arr.my_count(5) { |ele, arg| ele === arg }).to eql(0)
+          expect(test_arr.my_count(5) { |ele, arg| ele == arg }).to eql(0)
         end
       end
     end
@@ -157,7 +157,7 @@ RSpec.describe 'Enumerable' do
     end
 
     context ' when one element is zero' do
-      let(:arr ) {[1, 3, 0]}
+      let(:arr) { [1, 3, 0] }
       it 'should be equal to zero' do
         expect(arr.multiply_els { |sum, ele| sum * ele }).to be 0
       end
